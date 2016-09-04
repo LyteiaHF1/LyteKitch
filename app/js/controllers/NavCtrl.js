f6dc5931bbf8da38230b816bd13690ca89c3fb77
Lyteia Kitchen <lytehighfashion@gmail.com>
2016-09-04 05:45:18 -0400

    Old Commits

---
+++ app/js/controllers/NavCtrl.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/*global App*/
+
+app.controller('NavCtrl', function ($scope, $location, $routeParams, $anchorScroll, anchorSmoothScroll) {
+
+	console.log($routeParams.page);
+
+	if($routeParams.page === 'home'){
+		// $location.hash('home');
+		// $anchorScroll();
+		anchorSmoothScroll.scrollTo('home');
+		$location.hash('');
+	}else if($routeParams.page === 'projects'){
+		// $location.hash('projects');
+		// $anchorScroll();
+		anchorSmoothScroll.scrollTo('projects');
+		$location.hash('');
+	}else if($routeParams.page === 'about'){
+		// $location.hash('about');
+		// $anchorScroll();
+		anchorSmoothScroll.scrollTo('about');
+		$location.hash('');
+	}else if($routeParams.page === 'contact'){
+		// $location.hash('contact');
+		// $anchorScroll();
+		anchorSmoothScroll.scrollTo('contact');
+		$location.hash('');
+	}
+
+});
+
